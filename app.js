f7092b9a54607bb533db2a8f7e5b85dc1747e17a
Giuseppe Ursino <giursino@gmail.com>
2020-03-21 16:58:04 +0100

    4 col on row + retab

--- app.js
+++ app.js
@@ -1,33 +1,33 @@
 
 $(function() {
-	var undo = false;
+  var undo = false;
 
-	$(".button").on("click", function() {
+  $(".button").on("click", function() {
 
-		$verdi = Number($(this).attr("value"));
-		$sum = Number($("#sum").text());
+    $verdi = Number($(this).attr("value"));
+    $sum = Number($("#sum").text());
 
-		$count = Number($(this).find("span").text());
+    $count = Number($(this).find("span").text());
 
-		if (undo) {
-			if ($count) {
-				$sum = $sum - $verdi;
-				$count = $count - 1;
-			}
-		}
-		else {
-			$sum = $verdi + $sum;
-			$count = $count + 1;
-		}
+    if (undo) {
+      if ($count) {
+        $sum = $sum - $verdi;
+        $count = $count - 1;
+      }
+    }
+    else {
+      $sum = $verdi + $sum;
+      $count = $count + 1;
+    }
 
-		$("#sum").empty().text($sum);
-		$(this).find("span").empty().text($count);
+    $("#sum").empty().text($sum);
+    $(this).find("span").empty().text($count);
 
-		undo = false;
-	});
+    undo = false;
+  });
 
   $(".dec").on("click", function() {
-		undo = true;
+    undo = true;
   });
 
 });
@@ -35,9 +35,9 @@ $(function() {
 
 
 $(function() {
-	$("#cancel").on("click", function() {
-		$("#sum").empty().text("0");
-		$(".button span").empty().text("0");
-		return false;
-	});
+  $("#cancel").on("click", function() {
+    $("#sum").empty().text("0");
+    $(".button span").empty().text("0");
+    return false;
+  });
 });
